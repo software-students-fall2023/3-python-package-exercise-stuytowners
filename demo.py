7942dc9e21c6829532a7913f14f7a5bbd33ba002
Comeom Rice <cch9399@stern.nyu.edu>
2023-11-06 12:52:56 -0500

    changed from langchain to openai

--- demo.py
+++ demo.py
@@ -1,15 +1,24 @@
 from my_module import functions
 
 def main():
-    functions.joke()
-    functions.haiku()
-    functions.compliment()
-    functions.email()
+    functions.gptchat()
+    functions.gptchat(subject="what is your name")
+    functions.gptchat("joke")
+    functions.gptchat("joke", "software engineers")
+    functions.gptchat("haiku")
+    functions.gptchat("haiku", "software engineers")
+    functions.gptchat("compliment")
+    functions.gptchat("compliment", "software engineers")
+    functions.gptchat("email")
+    functions.gptchat("email", "software engineers")
+    # functions.haiku()
+    # functions.compliment()
+    # functions.email()
 
-    functions.joke("software engineers")
-    functions.haiku("carbon dioxide")
-    functions.compliment("mustache")
-    functions.email("tell my professor I can't come to class today because")
+    # functions.joke("software engineers")
+    # functions.haiku("carbon dioxide")
+    # functions.compliment("mustache")
+    # functions.email("tell my professor I can't come to class today because")
 
 
 
--- my_module/functions.py
+++ my_module/functions.py
@@ -1,9 +1,19 @@
-from langchain.llms import OpenAI
-llm = OpenAI(model_name="text-davinci-003")
+import openai
+
+def llm(question):
+   response = openai.Completion.create(
+   engine = "text-davinci-003",
+   prompt = question,
+   max_tokens=200,
+   temperature=0.99
+   )
+   answer = response.choices[0].text.strip()
+
+   return answer
 
 preset = "You are the most chill cs professor in the world, Professor Foo Barstein. You are talking to me, a student in your class."
 
-def gptchat(type, subject=None, preset=preset):
+def gptchat(type=None, subject=None, preset=preset):
     if (type=="joke"):
         if (subject):
             response=llm(preset + "tell me a funny joke about " + subject)
@@ -24,6 +34,11 @@ def gptchat(type, subject=None, preset=preset):
             response=llm(preset + "write an email about " + subject)
         else:
             response=llm(preset + "write a random email to send to my boss")
+    else:
+        if (subject):
+            response=llm(preset + subject)
+        else:
+            response=llm(preset + "say something random")
     print(response)
 
 def cowtalk():
