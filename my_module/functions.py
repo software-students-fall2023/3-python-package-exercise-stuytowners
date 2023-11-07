6a804fb593128050b5bb5b8fee4d1da8bfcfe213
Comeom Rice <cch9399@stern.nyu.edu>
2023-11-07 16:47:00 -0500

    changed openai api

--- my_module/functions.py
+++ my_module/functions.py
@@ -1,7 +1,8 @@
-import openai
+from openai import OpenAI
+client = OpenAI()
 
 def llm(question):
-    response = openai.ChatCompletion.create(
+    completion = client.chat.completions.create(
         model = "gpt-3.5-turbo",
         messages=[
         {"role": "user", "content": question},
@@ -9,8 +10,7 @@ def llm(question):
         max_tokens=200,
         temperature=0.99
     )
-    answer = response['choices'][0]['message']['content']
-
+    answer = completion.choices[0].message.content
     return answer
 
 preset = "You are the most chill cs professor in the world, Professor Foo Barstein. You are talking to me, a student in your class."
